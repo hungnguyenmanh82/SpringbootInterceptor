d99de3bf28844d9a945676f2ecd4fc0ef817aaae
DESKTOP-LBOHS1J\hungnm2 <hung.nguyen@netvisiontel.com>
2018-05-25 14:56:47 +0700

    interceptor in Springboot is the same at servlet filter in J2EE.

---
+++ src/main/java/com/example/demo/SpringBootInterceptorApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootInterceptorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootInterceptorApplication.class, args);
+	}
+}
